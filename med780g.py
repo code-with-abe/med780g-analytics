e9400ce243d781f8f6d3998ad6959af9b2e4b6c2
Abe <abraham.pabbathi@databricks.com>
2023-09-30 21:09:34 -0500

    commit

--- med780g.py
+++ med780g.py
@@ -1,25 +1,25 @@
 import streamlit as st
 import pandas as pd
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 
 st.title("Medtronic 780g Data Analysis")
 
 
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+# with st.sidebar:
+#     openai_api_key = st.text_input("OpenAI API Key", type="password")
+#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+# def generate_response(input_text):
+#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+#     st.info(llm(input_text))
 
 
-with st.form("my_form"):
-    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-    elif submitted:
-        generate_response(text)
+# with st.form("my_form"):
+#     text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
+#     submitted = st.form_submit_button("Submit")
+#     if not openai_api_key:
+#         st.info("Please add your OpenAI API key to continue.")
+#     elif submitted:
+#         generate_response(text)
