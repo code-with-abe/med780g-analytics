2d496c5093de102dc9f3ae7d60ef1a553ca98f9d
Abe <abraham.pabbathi@databricks.com>
2023-10-02 17:21:27 -0500

    Commit

--- med780g.py
+++ med780g.py
@@ -6,9 +6,9 @@ st.title("Medtronic 780g Data Analysis")
 
 
 
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", type="password")
-#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
 
 # def generate_response(input_text):
