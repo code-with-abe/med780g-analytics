cd313fe61b3882fe8e040c013da3869be2d96eb8
Abe <74078480+code-with-abe@users.noreply.github.com>
2023-10-02 19:28:05 -0500

    Update med780g.py

--- med780g.py
+++ med780g.py
@@ -1,25 +1,18 @@
 import streamlit as st
-import pandas as pd
 from langchain.llms import OpenAI
+st.set_page_config(page_title="Smart Diabetes")
+st.title('Abe's Med780g Analytics')
 
-st.title("Medtronic 780g Data Analysis")
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-
-
-# def generate_response(input_text):
-#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-#     st.info(llm(input_text))
-
-
-# with st.form("my_form"):
-#     text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
-#     submitted = st.form_submit_button("Submit")
-#     if not openai_api_key:
-#         st.info("Please add your OpenAI API key to continue.")
-#     elif submitted:
-#         generate_response(text)
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
